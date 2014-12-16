6112ebd41650b88162741c12eefdfa4c4d9a819c
Ryan Day <soldair@gmail.com>
2014-12-16 11:42:09 -0800

    adding uno example and fixing v1 file name

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 //default to v2
 module.exports = require('./stk500v2.js');
 module.exports.v2 = module.exports;
-module.exports.v1 = require('./stk500.js');
+module.exports.v1 = require('./stk500v1.js');
 
